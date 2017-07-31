90761d209d58777d892b545dc3e3e0dff28d60a9
dchandlerp <chandler.prince@gmail.com>
2017-07-31 11:17:17 -0500

    Finished Classroom Controller. Based on CheeseController in Cheese MVC.

--- src/main/java/com/chandler/classroommanagementapp/controllers/ClassroomController.java
+++ src/main/java/com/chandler/classroommanagementapp/controllers/ClassroomController.java
@@ -1,12 +1,17 @@
 package com.chandler.classroommanagementapp.controllers;
 
+import com.chandler.classroommanagementapp.model.forms.Classroom;
 import com.chandler.classroommanagementapp.models.data.ClassroomDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * Created by davidprince on 7/31/17.
@@ -19,10 +24,29 @@ public class ClassroomController {
     @Autowired
     private ClassroomDAO classroomDAO;
 
-    @RequestMapping(value="", method = RequestMethod.GET)
-    public String index(Model model, @RequestParam(defaultValue = "0") int id){
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, @RequestParam(defaultValue = "0") int id) {
         model.addAttribute("title", "Classrooms");
         model.addAttribute("Classrooms", classroomDAO.findAll());
         return "classroom/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Classroom());
+        model.addAttribute("title", "Add Class");
+        return "classroom/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Classroom classroom, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Classroom");
+            return "classroom/add";
+        }
+
+        classroomDAO.save(classroom);
+        return "redirect:";
     }
 }
