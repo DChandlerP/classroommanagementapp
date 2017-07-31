36b28c861c3dcaaa934198b7c3723ed9ff725faf
dchandlerp <chandler.prince@gmail.com>
2017-07-31 11:49:52 -0500

    Created fragments.html template

--- src/main/java/com/chandler/classroommanagementapp/controllers/StudentController.java
+++ src/main/java/com/chandler/classroommanagementapp/controllers/StudentController.java
@@ -1,7 +1,33 @@
 package com.chandler.classroommanagementapp.controllers;
 
+import com.chandler.classroommanagementapp.models.data.ClassroomDAO;
+import com.chandler.classroommanagementapp.models.data.StudentDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by davidprince on 7/31/17.
  */
+@Controller
+@RequestMapping("")
 public class StudentController {
+
+    @Autowired
+    StudentDAO studentDAO;
+
+    @Autowired
+    ClassroomDAO classroomDAO;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("students", studentDAO.findAll());
+        model.addAttribute("title", "All Students: ");
+        return "index";
+    }
+
+
+
 }
